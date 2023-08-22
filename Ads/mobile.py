cf8b8aeac1b583e8b1f12c7bccfd0b66ad7d02a6
snedev <46651489+Sashini336@users.noreply.github.com>
2023-08-22 17:25:54 +0300

    mobile.bg update

--- Ads/mobile.py
+++ Ads/mobile.py
@@ -5,74 +5,62 @@ from bs4 import BeautifulSoup
 
 def extract_info(soup, link):
     title = None
-    year = None
-    millage = None
-    transmission = None
-    horsepower = None
-    color = None
-    
-
-    div_with_margin = soup.find('div', style='margin-top:17px;')
-
-    table = div_with_margin.find('table')
-    
-    title_h1 = soup.find('h1')
-    
-    title = title_h1.text.strip()
-
-    price_div = soup.find('span' , id="details_price")
-    
-    price_text = price_div.text.strip()
-    
-    price_numbers = re.findall(r'\d+', price_text)
-    
-    price = ''.join(price_numbers)
-    
-    main_image_holder = soup.find('img', id="bigPictureCarousel")
-    
-    image = main_image_holder.get('src')
-    
-    images_holder = soup.find('div', id="pictures_moving_details_small")
-    
-    a_elements = images_holder.find_all('a')
-    
+    price = None
     image_urls = []
 
-    for a_element  in a_elements:
-        image = a_element.get('data-link')
+    title_h1 = soup.find('h1')
+    if title_h1:
+        title = title_h1.text.strip()
+
+    price_div = soup.find('span', id="details_price")
+    if price_div:
+        price_text = price_div.text.strip()
+        price_numbers = re.findall(r'\d+', price_text)
+        price = ''.join(price_numbers)
+
+    main_image_holder = soup.find('img', id="bigPictureCarousel")
+    if main_image_holder:
+        image = main_image_holder.get('src')
         if image:
             image_urls.append(image)
-            
+
+    images_holder = soup.find('div', id="pictures_moving_details_small")
+    if images_holder:
+        a_elements = images_holder.find_all('a')
+        for a_element in a_elements:
+            image = a_element.get('data-link')
+            if image:
+                image_urls.append(image)
+
+    specs_dict = {}
+
     more_info_specs = soup.find('ul', class_="dilarData")
-    
-    li_elements = more_info_specs.find_all('li')
+    if more_info_specs:
+        li_elements = more_info_specs.find_all('li')
 
-    label_to_index = {
-        "year": 1,
-        "fuel_type": 3,
-        "horsepower": 5,
-        "transmission": 9,
-        # "type": 11,
-        "millage": 15,
-        "color": 17,
-    }
+        label_to_index = {
+            "year": 1,
+            "fuel_type": 3,
+            "horsepower": 5,
+            "transmission": 9,
+            "millage": 15,
+            "color": 17,
+        }
 
-    specs_dict = {}  
-
-    for label, index in label_to_index.items():
-        info_value = li_elements[index].text.strip()
-        specs_dict[label] = info_value
-        
+        for label, index in label_to_index.items():
+            if index < len(li_elements):
+                info_value = li_elements[index].text.strip()
+                specs_dict[label] = info_value
 
     return {
         'url': link,
         'title': title,
         'price': price,
-        'main_image': image,
         'image_urls': image_urls,
-        **{key: value for key, value in specs_dict.items() if key in ('year', 'fuel_type', 'horsepower', 'transmission', 'millage', 'color')}
-        
+        **specs_dict  
     }
+
+
 def scrape_single_ad_mobile(url):
     try:
         headers = {
@@ -86,8 +74,15 @@ def scrape_single_ad_mobile(url):
 
         soup = BeautifulSoup(response.content, 'html.parser')
         info = extract_info(soup, url)
-        return info
+
+        if info['title'] and info['price'] and info['image_urls']:
+            return info
+        else:
+            print(f"Required information (title, price, and images) not found for URL: {url}")
+            return None
 
     except requests.exceptions.RequestException as e:
         print(f"Error dokato se opitva da scrapne datata:  {e}")
         return None
+
+
